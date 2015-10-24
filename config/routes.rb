3c20ce2dae26b054a080ecaa2d1a5924ac971f41
Craig Banton <c.banton12@gmail.com>
2015-10-24 13:39:09 -0400

    added home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+
+  devise_for :users
+  devise_for :views
+  devise_for :installs
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
